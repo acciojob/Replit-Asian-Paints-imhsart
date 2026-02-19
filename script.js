bc822592cce3e1c0ddaeadfccca0f811045733ba
Nitin Rana <nitinrana2519@gmail.com>
2026-02-19 16:13:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 //your JS code here. If required.
-let inputId = document.getElementById('block_id')
-let colorId = document.getElementById('colour_id')
+let gridInput = document.getElementById('block_id')
+let colorInput = document.getElementById('colour_id')
 let changeBtn = document.getElementById('change_button')
 let resetBtn = document.getElementById('reset_button')
 let gridItems = document.querySelectorAll('.grid-item')
 	
 changeBtn.addEventListener('click',() => {
 
-	let gridnum = Number(inputId.value);
+	let gridnum = Number(gridInput.value);
 	if(!gridnum) return;
-	let color = colorId.value
+	let color = colorInput.value
 
-	gridItems[gridnum-1].style.backgroundColor = `${color}`;
+	gridItems[gridnum-1].style.backgroundColor = color;
 	for(let item of gridItems){
 		if(!(item.id == (gridnum))){
 		  item.style.backgroundColor = 'transparent';
 		}
 	}
+	gridInput.value = ''
+	colorInput.value= ''
 })
 resetBtn.addEventListener('click', ()=>{
 	gridItems.forEach(item => {
 		item.style.backgroundColor = 'transparent';
 	});
-	inputId.value = ''
+	gridInput.value = ''
+	colorInput.value= ''
 })
 	
